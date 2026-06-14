d679ad8ea55e9a1bd4408a5f0e29d858c0a3990c
Atsui04 <bushkog92@gmail.com>
2026-06-15 02:43:14 +0300

    feat: add arrow keys navigation and focus-visible styles for keyboard accessibility

--- script.js
+++ script.js
@@ -25,4 +25,32 @@ const toggleActive = (e) => {
   questionContent.classList.toggle("question__content--active");
 };
 
+const toggleKeyboard = (e) => {
+  const target = e.target.closest(".btn");
+
+  if (!target) return null;
+
+  const currentQuestion = target.closest(".question");
+
+  if (e.key === "ArrowDown") {
+    const nextQuestion = currentQuestion.nextElementSibling;
+
+    if (nextQuestion) {
+      const newTarget = nextQuestion.querySelector(".btn");
+      newTarget.focus();
+    }
+  }
+
+  if (e.key === "ArrowUp") {
+    const prevQuestion = currentQuestion.previousElementSibling;
+
+    if (prevQuestion) {
+      const newTarget = prevQuestion.querySelector(".btn");
+      newTarget.focus();
+    }
+  }
+};
+
 questionList.addEventListener("click", toggleActive);
+
+questionList.addEventListener("keydown", toggleKeyboard);
